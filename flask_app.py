c64605b9be1b83c96638ef4f0a694497cba0311f
Wissam Sawah <42570924+WissamSawah@users.noreply.github.com>
2019-10-13 18:55:00 +0200

    Flask_app.p contains only server routes.
    I have moved out all the functions from Flask_app to smaller modules. And now this module contains only server routes.

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,9 @@
 import base64
 import sys
-import tempfile
 import cv2
 import time
-import argparse
 import datetime
 import re
-
 
 import numpy as np
 from queue import Queue
@@ -16,6 +13,7 @@ import threading
 from fps import FPS
 from stream import liveStream
 from detector import ObjectDetector
+from objects import detect_objects_webcam
 
 MODEL_BASE = 'models/research'
 sys.path.append(MODEL_BASE)
@@ -91,54 +89,6 @@ def worker(input_q, output_q):
 
     fps.stop()
     sess.close()
-
-# detector for web camera
-def detect_objects_webcam(image_np, sess, detection_graph):
-    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-    # screen = cv2.resize((0,40,1280,745), (800,450))
-    image_np_expanded = np.expand_dims(image_np, axis=0)
-    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-
-    # Each box represents a part of the image where a particular object was detected.
-    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-
-    # Each score represent how level of confidence for each of the objects.
-    # Score is shown on the result image, together with the class label.
-    scores = detection_graph.get_tensor_by_name('detection_scores:0')
-    classes = detection_graph.get_tensor_by_name('detection_classes:0')
-    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-
-    # Actual detection.
-    (boxes, scores, classes, num_detections) = sess.run(
-        [boxes, scores, classes, num_detections],
-        feed_dict={image_tensor: image_np_expanded})
-
-    # Visualization of the results of a detection.
-    rect_points, class_names, class_colors = draw_boxes_and_labels(
-        boxes=np.squeeze(boxes),
-        classes=np.squeeze(classes).astype(np.int32),
-        scores=np.squeeze(scores),
-        category_index=client.category_index,
-        min_score_thresh=.5,
-
-    )
-    for i,b in enumerate(boxes[0]):
-        if classes[0][i] == 3 or classes[0][i] == 6 or classes[0][i] == 8:
-            if scores[0][i] >= 0.5:
-                mid_x = (boxes[0][i][1]+boxes[0][i][3])/2
-                mid_y = (boxes[0][i][2]+boxes[0][i][3])/2
-                apx_distance = round(((1 - (boxes[0][i][3] - boxes[0][i][1]))**4),1)
-
-                cv2.putText(image_np, '{}'.format(apx_distance), (int(mid_x*800),int(mid_y*450)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,255), 2)
-                # gray = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
-
-                if apx_distance == 0:
-                    # if mid_x > 0.3 and mid_x < 0.7:
-                    # if mid_x > 0.3 and mid_x < 0.7:
-                    cv2.putText(image_np, 'WARNING!!!', (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,0,255), 3)
-
-    return dict(rect_points=rect_points, class_names=class_names, class_colors=class_colors)
-
 
 
 class VideoForm(Form):
@@ -287,10 +237,6 @@ def send_mail():
         return 'Mail sent!'
     except Exception:
         return("error")
-
-
-
-
 
 # Just camera without the interface incase someone wanna use it to another application.
 
